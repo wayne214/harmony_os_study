301cfc870001d1520896130a4f44653f03ef4a5e
wangjunwei <>
2024-04-02 17:03:53 +0800

    增加了几个工具类

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,2 +1,36 @@
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
 export { appTasks } from '@ohos/hvigor-ohos-plugin';
+
+// import {appTasks} from '@ohos/hvigor-ohos-plugin'
+
+// 自定义task
+// function myTask() {
+//   return {
+//     pluginId: 'myTask',
+//     apply(pluginContext) {
+//       pluginContext.registerTask({
+//         name: 'HelloWorld',
+//         run: (taskContext) => {
+//           console.log("hello world")
+//         }
+//       })
+//     }
+//   }
+// }
+//
+// // 自定义plugin
+// function customPlugin(): HvigorPlugin {
+//   return {
+//     pluginId: 'customPlugin',
+//     apply(node: HvigorNode) {
+//       // 插件主体
+//       console.log('hello customPlugin');
+//     }
+//   }
+//
+// }
+
+// export default {
+//   system: appTasks,
+//   // plugins: [myTask]
+// }
